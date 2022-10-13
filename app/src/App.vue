aef75540289f5f9a9c1fbe1650eb18a1d438892b
1053710934@qq.com <1053710934@qq.com>
2022-10-13 21:52:43 +0800

    10131

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,12 +1,20 @@
 <template>
   <div>
+    <Header></Header>
     我是跟组件
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
+//引入
+import Header from './components/Header'
+import Footer from './components/Footer'
 export default {
   name: 'App',
+  components:{
+    Header,Footer
+  }
   
 }
 </script>
