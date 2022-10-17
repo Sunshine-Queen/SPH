2e5453ffe9153da61e3ca8ba334e85777ca5a86e
Esther <1053710934@qq.com>
2022-10-17 16:18:44 +0800

    1017

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
     <Header></Header>
-    我是跟组件
-    <Footer></Footer>
+    <!-- 路由子组件出口的地方 -->
+    <router-view></router-view>
+    <!-- 在Home、Search显示，在登录和注册隐藏 -->
+    <Footer v-show="$route.meta.show"></Footer>
   </div>
 </template>
 
